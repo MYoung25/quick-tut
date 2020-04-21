b51e91df3cf2bf403693801b857d27da3f200212
Michael Young <michael@inspiringservice.org>
2020-04-21 15:08:15 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const http = require('http')
+const app = require('./app')
+
+const server = http.createServer(app)
+const port = process.env.PORT ? process.env.PORT : 8080
+server.on('listening', () => {
+    console.log(`Listening on: ${port}`)
+})
+
+server.listen(port)
